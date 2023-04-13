75e4366a823f559397105d617e156c250c967a8b
Jakub Cierlik <jakub.cierlik@amartus.com>
2023-04-13 13:19:17 +0200

    updates

--- k3s-rds-scripts/k3s_install.sh
+++ k3s-rds-scripts/k3s_install.sh
@@ -8,5 +8,6 @@ sudo cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/kube/.config
 sudo chown -R ubuntu:ubuntu /home/ubuntu/kube
 
 # Save K3S kubeconfig
-kubeconfig=$(sudo cat /etc/rancher/k3s/k3s.yaml)
+kubeconfig_raw=$(sudo cat /etc/rancher/k3s/k3s.yaml)
+kubeconfig="${kubeconfig_raw/127.0.0.1/${PUBLIC_IP}}"
 ctx instance runtime-properties "kubeconfig" "${kubeconfig}"
