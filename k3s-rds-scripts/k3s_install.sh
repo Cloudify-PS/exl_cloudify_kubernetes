012a8ad7a51142a2817152a76e3b5014a6557cd5
Jakub Cierlik <jakub.cierlik@amartus.com>
2023-04-13 13:01:34 +0200

    updates

--- k3s-rds-scripts/k3s_install.sh
+++ k3s-rds-scripts/k3s_install.sh
@@ -4,8 +4,8 @@
 # Install K3S
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.26.1+k3s1 sh -s - server --datastore-endpoint="postgres://k3s:k3Sp4sSw0rD@${RDS_HOSTNAME}:${RDS_PORT}/k3s"
 mkdir -p /home/ubuntu/kube
-chown -R ubuntu:ubuntu /home/ubuntu/kube
-cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/kube/.config
+sudo cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/kube/.config
+sudo chown -R ubuntu:ubuntu /home/ubuntu/kube
 
 # Save K3S kubeconfig
 kubeconfig=$(sudo cat /etc/rancher/k3s/k3s.yaml)
